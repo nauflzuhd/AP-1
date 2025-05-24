722d9661bc6721ea21b37fcc1313996ee18a40d3
nauflzuhd <nauflzuhd@gmail.com>
2025-05-24 14:49:41 +0700

    Pertemuan 4 : Perulangan

--- pertemuan 4/latihan2.cpp
+++ pertemuan 4/latihan2.cpp
@@ -1,16 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // header untuk C++
+using namespace std; // agar tidak menulis std diprogram
 
-int main() {
-    system("cls");
+int main() { // memulai main program
+    system("cls"); // agar layar bersih
+    
+    string kalimat; // variabel yang digunakan
+    cout << "Masukkan kalimat: "; // output
+    getline(cin, kalimat); // input menggunakan getline agar bisa membaca karakter spasi
 
-    string kalimat;
-    cout << "Masukkan kalimat: "; getline(cin, kalimat);
-
-    for (int i = 0; i < kalimat.length(); i++) {
-        if (kalimat[i] == 'a' || kalimat[i] == 'e' || kalimat[i] == 'i' || kalimat[i] == 'o' || kalimat[i] == 'u'){
-            kalimat[i] = toupper(kalimat[i]);
-        }
-    }
-    cout << "Kalimat hasil konversi: " << kalimat << endl;
-}
+    for (int i = 0; i < kalimat.length(); i++) { // for looping untuk i = 0,dieksekusi jika i kurang dari pajangan karakter kalimat, i di increment
+        if (kalimat[i] == 'a' || kalimat[i] == 'e' || kalimat[i] == 'i' || kalimat[i] == 'o' || kalimat[i] == 'u') { // jika i kurang dari panjang karakter kalimat dan jika kalimat indeks ke-i merupakan huruf vokal kecil maka
+            kalimat[i] = toupper(kalimat[i]); // kalimat indeks ke-i akan dieksekusi
+        } // akhir if statement
+    } // akhir for looping
+    cout << "Kalimat hasil konversi: " << kalimat << endl; // output kalimat lalu pindah baris
+} // akhir program
