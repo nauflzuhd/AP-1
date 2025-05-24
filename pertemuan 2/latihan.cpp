2899cca1e58b9906d2a699c30f20e3a782b11c31
nauflzuhd <nauflzuhd@gmail.com>
2025-05-24 13:41:52 +0700

    Pertemuan 2 : Penggunaan Operator

--- pertemuan 2/latihan.cpp
+++ pertemuan 2/latihan.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
-using namespace std;
-main() {
-    float jari, volume, luas;
-    system("cls");
-    cout << "Masukkan jari jari bola : ";
-    cin >> jari;
-    volume = float(4)/float(3) *3.14 * jari * jari * jari;
-    luas = 4 * 3.14 * jari * jari;
-    cout << "Volume bola adalah : " << volume <<endl;
-    cout << "Luas permukaan bola adalah : " << luas <<endl;
-}
+#include <iostream> // header C++
+using namespace std; // agar tidak memakai std diprogram
+
+main() { // memulai program
+    float jari, volume, luas; // variabel yang digunakan program
+    system("cls"); // agar tampilan layar bersih
+    cout << "Masukkan jari jari bola : "; // output
+    cin >> jari; // input jari
+    volume = float(4)/float(3) *3.14 * jari * jari * jari; // float digunakan agar hasilnya tidak dibulatin ke bawah ( bisa muncul dalam bentuk koma ) 
+    luas = 4 * 3.14 * jari * jari; // menghitung nilai luas
+    cout << "Volume bola adalah : " << volume <<endl; // output variabel volume dan pindah baris
+    cout << "Luas permukaan bola adalah : " << luas <<endl; // output variabel luas dan pindah baris
+} // akhir program
