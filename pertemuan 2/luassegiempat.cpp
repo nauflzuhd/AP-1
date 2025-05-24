56705ee3404edcfb1e2b55587bc0fb1aab228561
nauflzuhd <nauflzuhd@gmail.com>
2025-05-24 13:44:27 +0700

    Pertemuan 2 : Penggunaan Operator

--- pertemuan 2/luassegiempat.cpp
+++ pertemuan 2/luassegiempat.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    float luas, p, l;
-    cout << "Masukkan panjang : ";
-    cin >> p;
+#include <iostream> // header pada C++
+using namespace std; // agar tidak memakai std diprogram
 
-    cout << "Masukkan lebar : ";
-    cin >> l;
+int main() { // untuk memulai program
+    float luas, p, l; // variabel yang digunakan
+    cout << "Masukkan panjang : "; // output
+    cin >> p; // input p
 
-    luas = p * l;
-    cout << "Luas = "<< luas << endl;
-    return 0;
-}
+    cout << "Masukkan lebar : "; // output
+    cin >> l; // input l
+
+    luas = p * l; // menghitung luas
+    cout << "Luas = "<< luas << endl; // output luas dan pindah baris
+    return 0; // menghentikan program ( mengembalikan nilai 0 pada program utama )
+} // akhir program
