e3457142e57a069fa9cb61d97ba2cd5926c400da
nauflzuhd <nauflzuhd@gmail.com>
2025-05-24 13:37:51 +0700

    Pertemuan 1 : Pengenalan & Operasi Input

--- pertemuan 1/belajar.cpp
+++ pertemuan 1/belajar.cpp
@@ -1,29 +1,38 @@
 #include <iostream> //header untuk cpp /* komentar beberapa baris */
 #include <conio.h> //header untuk getche,getch
 using namespace std; //agar tidak menggunakan std di dalam main program
-main ()
-{
-    string nama;
+
+main () { // memulai program
+    string nama; // baris 6 sampai 9 variabel yang digunakan
     char kom,kelamin;
     int nim;
     float ip;
 
-    cout<<"Hello world!"<<endl;
-    cout<<"Masukkan Nama : ";  //cin>>nama;
-    getline(cin,nama); //agar karakter spasi bisa terbaca
-    cout<<"Masukkan Kom            : ";cin>>kom;
-    cout<<"Masukkan NIM            : ";cin>>nim;
-    cout<<"Masukkan IP             : ";cin>>ip;
-    cout<<"Masukkan jenis kelamain : ";
+    cout<<"Hello world!"<<endl; // fungsi keluaran pada c++, endl untuk pindah baris
+    
+    cout<<"Masukkan Nama : ";  //cin>>nama; juga bisa digunakan, tapi tidak bisa membaca karakter spasi
+    getline(cin,nama); //fungsi masukan pada c++ agar karakter spasi bisa terbaca
+    
+    cout<<"Masukkan Kom            : "; // output
+    cin >> kom; // fungsi masukan pada c++
+    
+    cout<<"Masukkan NIM            : "; // output
+    cin >> nim; // input
+    
+    cout<<"Masukkan IP             : "; // output
+    cin >> ip; // input
+    
+    cout<<"Masukkan jenis kelamain : "; // output
     kelamin = getche(); //agar karakter langsung tampil jadi ga perlu tekan enter
-    cout<<endl;
+    cout << endl; // untuk pindah baris
 
-    cout<<"\nHalo "<<nama<<endl;
-    cout<<"KOM Kamu adalah           : "<<kom<<endl;
-    cout<<"NIM Kamu adalah           : "<<nim<<endl;
-    cout<<"IP Kamu adalah            : "<<ip<<endl;
-    cout<<"Jenis kelamin kamu adalah : ";
-    putchar(kelamin); //untuk menampilkan karakter jenis kelamin
+    /* Output pada C++ */
+    cout << "\nHalo "<< nama << endl; // output variabel yang telah diinisialisasi pada awal program ( output nama )
+    cout << "KOM Kamu adalah           : " << kom << endl; // output kom
+    cout << "NIM Kamu adalah           : " << nim << endl; // output nim
+    cout << "IP Kamu adalah            : " << ip << endl; // output nim
+    cout << "Jenis kelamin kamu adalah : "; // output
+    putchar(kelamin); //untuk menampilkan karakter jenis kelamin berdasarkan input kelamin menggunakan getche()
 
     getch(); //karakter yang diinput tidak muncul dilayar, tetapi disimpan dalam memori
-}
+} //akhir program
