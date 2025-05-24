af0afa28738dff96eee6f355bd7fefa3eb4a89cc
nauflzuhd <nauflzuhd@gmail.com>
2025-05-24 14:45:12 +0700

    Pertemuan 4 : Perulangan

--- pertemuan 4/latihan1.cpp
+++ pertemuan 4/latihan1.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // header untuk C++
+using namespace std; // agar tidak menulis std diprogram
 
-int main() {
-    string kalimat;
+int main() { // memulai main program
+    string kalimat; // baris 5 dan 6 adalah variabel yang digunakan
     int i;
 
-    cout << "Masukkan kalimat : ";
-    getline(cin, kalimat);
+    cout << "Masukkan kalimat : "; // output
+    getline(cin, kalimat); // input agar karakter spasi bisa dibaca
 
-    for (i = 0; i < kalimat.length(); i++) {
-        kalimat[i] = toupper(kalimat[i]);
-    }
+    for (i = 0; i < kalimat.length(); i++) { // for looping untuk nilai i = 0, jika i kurang dari panjang karakter kalimat, nilai i di increment
+        kalimat[i] = toupper(kalimat[i]); // jika i kurang dari panjang karakter kalimat maka kalimat indeks ke-i akan dikapitalkan
+    } // akhir for looping
 
-    cout << "Kalimat dalam huruf kapital : " << kalimat << endl;
-    return 0;
-}
+    cout << "Kalimat dalam huruf kapital : " << kalimat << endl; // output
+    return 0; // menghentikan program
+} // akhir program
