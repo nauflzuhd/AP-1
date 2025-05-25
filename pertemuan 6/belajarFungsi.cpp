1a83611c767bb601dc2580ed394b618b4da22855
nauflzuhd <nauflzuhd@gmail.com>
2025-05-25 10:16:53 +0700

    pertemuan 6 : Fungsi dan Header

--- pertemuan 6/belajarFungsi.cpp
+++ pertemuan 6/belajarFungsi.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // Header untuk C++
+using namespace std; // agar tidak menulis std di program
 
-void sapa(string nama) {
-    cout << "Halo " << nama << "! Selamat belajar c++!" << endl;
-}
+// fungsi tanpa balikan sapa
+void sapa(string nama) { // parameter formal nama
+    cout << "Halo " << nama << "! Selamat belajar c++!" << endl; // output
+} // akhir fungsi sapa
 
-int main () {
-    string namaPengguna = "Naufal";
-    sapa (namaPengguna);
-    return 0;
-}
-
+int main () { // memulai main program
+    string namaPengguna = "Naufal"; // variabel namaPengguna diisi Naufal
+    sapa(namaPengguna); // memanggil fungsi sapa dengan isi parameter aktual variabel namaPengguna
+    return 0; // menghentikan program
+} // akhir main program
