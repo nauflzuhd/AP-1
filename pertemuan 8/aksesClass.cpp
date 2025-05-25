163ebbc11aa4c340ab4bd401654f6a6cfbed6758
nauflzuhd <nauflzuhd@gmail.com>
2025-05-25 12:11:38 +0700

    pertemuan 8 : Struct & Class

--- pertemuan 8/aksesClass.cpp
+++ pertemuan 8/aksesClass.cpp
@@ -1,49 +1,49 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // Header untuk C++
+using namespace std; // agar tidak menulis std di program
 
-class ContohAkses {
-    private:
-        int privateVar;
+class ContohAkses { // class ContohAkses
+    private: // private hanya bisa di dalam class ini sendiri, mirip seperti variabel lokal
+        int privateVar; // variabel private
 
-    protected:
-        int protectedVar;
+    protected: // protected bisa untuk inheritage ( pewarisan )
+        int protectedVar; // variabel protected
 
-    public:
-        int publicVar;
+    public: // public bisa untuk semua, mirip seperti variabel global
+        int publicVar; // variabel public
 
     // Constructor
-    ContohAkses() {
-        privateVar = 1;
-        protectedVar = 2;
-        publicVar = 3;
-    }
+    ContohAkses() { // Constructor ContohAkses
+        privateVar = 1; // privateVar diisi dengan 1
+        protectedVar = 2; // protectedVar diisi dengan 2
+        publicVar = 3; // publicVar diisi dengan 3
+    } // akhir constructor
 
-    void tampilkanSemua() {
-        cout << "Akses dari dalam class : " << endl;
-        cout << privateVar << endl;
-        cout << protectedVar << endl;
-        cout << publicVar << endl;
-    }
-};
+    void tampilkanSemua() { // Fungsi tampilkanSemua
+        cout << "Akses dari dalam class : " << endl; // output lalu pindah baris
+        cout << privateVar << endl; // output privateVar
+        cout << protectedVar << endl; // output protectedVar
+        cout << publicVar << endl; // output publicVar
+    } // akhir fungsi tampilkanSemua
+}; // akhir class ContohAkses
 
-class Turunan : public ContohAkses {
-    public:
-    void aksesProtected() {
-        // cout << privateVar << endl;
-        cout << protectedVar << endl;
-        cout << publicVar << endl;
-    } 
-};
+class Turunan : public ContohAkses { // class Turunan, turunan dari ContohAkses
+    public: // bisa diakses semua
+    void aksesProtected() { // fungsi aksesProtected
+        cout << privateVar << endl; // tidak bisa karena privateVar adalah variabel private dari ContohAkses
+        cout << protectedVar << endl; // output protectedVar
+        cout << publicVar << endl; // output publicVar
+    } // akhir fungsi
+}; // akhir class Turunan
 
-int main() {
-    ContohAkses obj;
-    obj.tampilkanSemua();
-    cout << "\nAkses dari luar class : " << endl;
-    // cout << obj.privateVar << endl;
-    // cout << obj.protectedVar << endl;
-    cout << obj.publicVar << endl;
+int main() { // memulai main program
+    ContohAkses obj; // obj bertipe data ContohAkses(class)
+    obj.tampilkanSemua(); // memanggil fungsi tampilkanSemua pada class obj
+    cout << "\nAkses dari luar class : " << endl; // output lalu pindah baris
+    cout << obj.privateVar << endl; // tidak bisa karena private milik ContohAkses
+    cout << obj.protectedVar << endl; // tidak bisa karena protected milik ContohAkses
+    cout << obj.publicVar << endl; // output publicVar lalu pindah baris
 
-    cout << "\nAkses dari class Turunan : " << endl;
-    Turunan tur;
-    tur.aksesProtected();
-}
+    cout << "\nAkses dari class Turunan : " << endl; // output lalu pindah baris
+    Turunan tur; // tur bertipe data Turunan(turunan dari class)
+    tur.aksesProtected(); // memanggil fungsi aksesProtected
+} // akhir main program
