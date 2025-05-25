6c50459ca667c246794ca9d4320c34264bcadf7d
nauflzuhd <nauflzuhd@gmail.com>
2025-05-25 09:08:38 +0700

    Pertemuan 4 : Perulangan

--- pertemuan 4/loop.cpp
+++ pertemuan 4/loop.cpp
@@ -1,67 +1,82 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // Header untuk C++
+using namespace std; // agar tidak menulis std diprogram 
 
-int main() {
-    // goto label
+int main() { // memulai main program
+    // goto label, loncat ke label yang sudah ditentukan
     // Hello World, Fasilkom-TI, Ilmu Komputer, IKLC
     // -> Hello World, IKLC, Ilmu Komputer, Fasilkom-TI
 
-    // a:
-    //     cout << "Hello World" << endl;
-    //     goto d;
-    // b:
-    //     cout << "Fasilkom-TI" << endl;
-    //     return 0;
-    // c:
-    //     cout << "Ilmu Komputer" << endl;
-    //     goto b;
-    // d:
-    //     cout << "IKLC" << endl;
-    //     goto c;
+    a: // label a (1)
+        cout << "Hello World" << endl; // output
+        goto d; // pergi ke d
+    b: // label b (4)
+        cout << "Fasilkom-TI" << endl; // output
+        return 0; // menghentikan program
+    c: // label c (3)
+        cout << "Ilmu Komputer" << endl; // output
+        goto b;
+    d: // label d (2)
+        cout << "IKLC" << endl; // output
+        goto c; // pergi ke c
 
-    // menampilkan bilangan genap 10 -> 2 dengan goto
-    // int i = 10;
-    // genap:
-    // if (i % 2 == 0) 
-    //     cout << i << endl;
-    //     i--;
-    // if (i >= 2)
-    //     goto genap;
+    // menampilkan bilangan genap 10 -> 2 dengan goto ( perulangan dengan goto )
+    int i = 10; // variabel yang digunakan
+    genap: // label genap
+    if (i % 2 == 0)  // jika i bilangan genap maka
+        cout << i << endl; // output i
+        i--; // i di post decrement
+    if (i >= 2)  // jika i lebih besar atau sama dengan 2 maka pergi ke label genap
+        goto genap;
 
-    // statement while
-    // int i = 1;
-    // while(i <= 10) {
-    //     if (i % 2 == 0) {
-    //         cout << i << " ";
-    //     } i++;
-    // }
+    // statement while, perulangan selama kondisi terpenuhi
+    int i = 1; // variabel yang digunakan
+    while(i <= 10) { // ketika i kurang dari atau sama dengan 10 maka
+        if (i % 2 == 0) { // jika bilangan genap maka
+            cout << i << " "; // output i
+        } i++; // i di post increment
+    } // akhir while looping
 
-    // statement do-while
-    // int i = 1;
-    // do {
-    //     cout << i << endl;
-    // } while (i <= 0);
+    // statement do-while, perulangan dieksekusi terlebih dahulu kemudian baru dicek apakah kondisi terpenuhi ( perulangan minimal 1 kali )
+    int i = 1; // variabel yang digunakan
+    do { // awal do while looping
+        cout << i << endl; // output 1
+    } while (i <= 0); // mengecek apakah i kurang dari atau sama dengan 0, jika iya program akan diulang 
 
-    // statement for
+    // statement for, mirip seperti while tetapi lebih simpel karena inisialisasi, kondisi, dan increase sudah ditulis di awal statement
     // for (inisialisasi, kondisi, increase)
-    // for (int i = 1; i <= 10; i += 2) { // i = i + 2
-    //     cout << "Hello World" << endl;
-    // }
+    for (int i = 1; i <= 10; i += 2) { // variabel i = 1, selama i <= 10, increase i = i + 2
+        cout << "Hello World" << endl; // output
+    } // akhir for looping
 
-    // statement nested for
+    // statement nested for, for di dalam for
     // ***** 5 x 5
-    // for (int i = 1; i <= 5; i++) {
-    //     for (int j = 1; j <= 5; j++) {
-    //         cout << " * ";
-    //     }
-    //     cout << endl;
-    // }
+    for (int i = 1; i <= 5; i++) { // variabel i = 1, selama i <= 5, i increment
+        for (int j = 1; j <= 5; j++) { // variabel j = 1, selama j <= 5, j increment
+            cout << " * "; // output *
+        } // akhir for looping j
+        cout << endl; // pindah baris
+    } // akhir for looping i
+    
+    // Hasilnya
+    // *****
+    // *****
+    // *****
+    // *****
+    // *****
 
     // segitiga siku siku
-    for (int i = 1; i <= 5; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << " * ";
-        }
-        cout << endl;
-    }
-} 
+    for (int i = 1; i <= 5; i++) { // variabel i = 1, selama i <= 5, i increment
+        for (int j = 1; j <= i; j++) { // variabel j = 1, selama j <= i, j increment
+            cout << " * "; // output *
+        } // akhir for looping j
+        cout << endl; // pindah baris
+    } // akhir for looping i
+
+    // Hasilnya
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+    
+} // akhir program
