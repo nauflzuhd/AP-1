39072632073569685794dbcd74e15278207f76e8
nauflzuhd <nauflzuhd@gmail.com>
2025-05-25 10:35:10 +0700

    pertemuan 6 : Fungsi dan Header

--- pertemuan 6/header.cpp
+++ pertemuan 6/header.cpp
@@ -1,24 +1,23 @@
-#include <iostream> // bawaan
-#include <cmath> // bawaan
+#include <iostream> // Header untuk C++
+#include <cmath> // Header untuk library cmath
 #include "headerCustom.h" // header buatan sendiri
-u
+u // define dari using namespace std;
 
-int jumlah(int a, int b) {
-    tambah1
-}
+int jumlah(int a, int b) { // fungsi jumlah dengan parameter formal a dan b
+    tambah1 // define dari return a + b;
+} // akhir fungsi
 
-int kurang(int a, int b) {
-    kurang1
-}
+int kurang(int a, int b) { // fungsi kurang dengan parameter formal a dan b
+    kurang1 // define dari return a - b;
+} // akhir fungsi
+ 
+begin // define dari int main () { 
+    c // define dari system("CLS");
+    int n; // variabel yang digunakan
+    o l jumlah(2, 3) l e; // o define dari cout, l define dari <<, e define dari endl, baris ini memanggil fungsi jumlah dengan parameter aktual 2 dan 3
+    o l kurang(5, 3) l e; // o define dari cout, l define dari <<, e define dari endl, baris ini memanggil fungsi kurang dengan parameter aktual 5 dan 3
 
-begin
-    c
-    int n;
-    o l jumlah(2, 3) l e;
-    o l kurang(5, 3) l e;
-
-    double angka = 25.0;
-    double akar = sqrt(angka);
-    o l "Akar dari " l angka l " adalah " l akar l e;
-
-end
+    double angka = 25.0; // variabel yang digunakan
+    double akar = sqrt(angka); // variabel akar diisi dengan akar dari variabel angka
+    o l "Akar dari " l angka l " adalah " l akar l e; // o define dari cout, l define dari <<, e define dari endl, baris ini adalah output
+end // define dari }
