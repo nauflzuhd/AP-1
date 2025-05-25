cbf1642095e886eeaa9e9cd2982e335b4d2349e1
nauflzuhd <nauflzuhd@gmail.com>
2025-05-25 10:12:46 +0700

    Pertemuan 5 : Array & Vector

--- pertemuan 5/vector.cpp
+++ pertemuan 5/vector.cpp
@@ -1,27 +1,38 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include <iostream> // Header C++
+#include <vector> // Header library Vector
+using namespace std; // agar tidak menulis std diprogram
 
-int main () {
-    // Vector Declaration & Initialization
+    // Vector Declaration & Initialization, deklarasi dan inisialisasi vektor
+    /* bedanya vektor dan array */
+    /* 1. vektor hanya bisa satu dimensi, sedangkan array bisa multidimensi */
+    /* 2. vektor tidak dibatasi banyak datanya, sedangkan array perlu dibatasi */
+    /* 3. vektor bisa diisi nilainya kapan saja, sedangkan array diisi harus berurut */
+    /* 4. vektor bersifat dinamis, sedangkan array bersifat statis */
 
-    vector<string> nama_karyawan = {"Alya", "Parul", "Syukron", "Aurick", "Dzackwan"};
-    // for (string karyawan : nama_karyawan) {
-    //     cout << karyawan << endl;
-    // }
+int main () { // memulai main program
+    
+    vector<string> nama_karyawan = {"Alya", "Parul", "Syukron", "Aurick", "Dzackwan"}; // vektor nama_karyawan diisi
+    
+    // for looping untuk output isi dari vektor nama_karyawan
+    for (string karyawan : nama_karyawan) { // variabel karyawan diisi vektor nama_karyawan
+        cout << karyawan << endl; // output karyawan lalu pindah baris
+    } // akhir for looping
 
-    //add data to vector (push_back()) 
-    nama_karyawan.push_back("Dhafa");
+    // add data to vector push_back(), mengisi satu data di akhir
+    nama_karyawan.push_back("Dhafa"); // Dhafa diisi pada akhir vektor nama_karyawan
 
-    // for (int i=0; i < nama_karyawan.size(); i++) {
-    //     cout << nama_karyawan[i] << endl;
-    // }
+    // for looping untuk output isi dari vektor nama_karyawan
+    for (int i = 0; i < nama_karyawan.size(); i++) { // variabel i = 0, selama i < banyak data nama_karyawan, i increment
+        cout << nama_karyawan[i] << endl; // output nama_karyawan pada indeks ke-i
+    } // akhir for looping
 
-    // Delete data from vector(pop.bakc())
-    nama_karyawan.pop_back();
+    // Delete data from vector pop.back(), menghapus satu data di akhir
+    nama_karyawan.pop_back(); // menghapus satu data di akhir ( Dhafa yang tadi diisi dihapus )
 
-    nama_karyawan.erase(nama_karyawan.begin() + 3);
-     for (int i=0; i < nama_karyawan.size(); i++) {
-        cout << nama_karyawan[i] << endl;
-    }
-}
+    nama_karyawan.erase(nama_karyawan.begin() + 3); // menghapus isi vektor nama_karyawan pada urutan pertama ditambah 3
+    
+    // for looping untuk output isi dari vektor nama_karyawan
+     for (int i=0; i < nama_karyawan.size(); i++) { // variabel i = 0, selama i < banyak data dari nama_karyawan, i increment
+        cout << nama_karyawan[i] << endl; // output nama_karyawan pada indeks ke-i lalu pindah baris
+    } // akhir for looping
+} // akhir main program
